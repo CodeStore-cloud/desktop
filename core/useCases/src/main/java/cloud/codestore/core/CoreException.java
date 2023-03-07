0b9ec017fdabbbe731e335d57b9326645f52afc2
Franz Deschler <webmaster@codestore.cloud>
2023-03-07 18:52:47 +0100

    Implement existence-check when reading a code snippet

--- core/useCases/src/main/java/cloud/codestore/core/CoreException.java
+++ core/useCases/src/main/java/cloud/codestore/core/CoreException.java
@@ -1,8 +1,5 @@
 package cloud.codestore.core;
 
-import java.text.MessageFormat;
-import java.util.ResourceBundle;
-
 /**
  * Abstract domain exception that indicates that a business rule was violated.
  */
