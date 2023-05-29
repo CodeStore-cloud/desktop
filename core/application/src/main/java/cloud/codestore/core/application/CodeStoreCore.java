ca4189f10e1c4282904db8256c8a5b7edf94050e
Franz Deschler <webmaster@codestore.cloud>
2023-05-29 19:16:24 +0200

    Use package scanning

--- core/application/src/main/java/cloud/codestore/core/application/CodeStoreCore.java
+++ core/application/src/main/java/cloud/codestore/core/application/CodeStoreCore.java
@@ -1,15 +1,12 @@
 package cloud.codestore.core.application;
 
-import cloud.codestore.core.api.ApiConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 /**
  * The main class of the {CodeStore} Core.
  */
-@SpringBootApplication
-@Import({StartupListener.class, ApiConfiguration.class})
+@SpringBootApplication(scanBasePackages = "cloud.codestore.core")
 public class CodeStoreCore {
     public static void main(String[] args) {
         SpringApplication.run(CodeStoreCore.class, args);
