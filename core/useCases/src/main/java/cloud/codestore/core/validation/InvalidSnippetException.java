f651a0e41e503dc2c683ec95d5960a614b311ef9
Franz Deschler <webmaster@codestore.cloud>
2023-06-08 10:46:35 +0200

    Fix code style

--- core/useCases/src/main/java/cloud/codestore/core/validation/InvalidSnippetException.java
+++ core/useCases/src/main/java/cloud/codestore/core/validation/InvalidSnippetException.java
@@ -7,24 +7,20 @@ import java.util.Map;
 /**
  * Exception in case a code snippet that should be saved is invalid.
  */
-public class InvalidSnippetException extends CoreException
-{
+public class InvalidSnippetException extends CoreException {
     private final Map<SnippetProperty, String> validationMessages;
-    
-    InvalidSnippetException(Map<SnippetProperty, String> validationMessages)
-    {
+
+    InvalidSnippetException(Map<SnippetProperty, String> validationMessages) {
         super("error.invalidSnippet");
         this.validationMessages = validationMessages;
-        for(var entry : validationMessages.entrySet())
+        for (var entry : validationMessages.entrySet())
             entry.setValue(createMessage(entry.getValue()));
     }
-    
+
     /**
      * @return a map containing an error message for each invalid snippet property.
-     * The keys of the map are the names of the properties, the values are the corresponding error message.
      */
-    public Map<SnippetProperty, String> getValidationMessages()
-    {
+    public Map<SnippetProperty, String> getValidationMessages() {
         return validationMessages;
     }
 }
