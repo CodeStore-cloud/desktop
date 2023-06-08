530f1d24fbf86ffa24ebfb69be2944383c530316
Franz Deschler <webmaster@codestore.cloud>
2023-06-08 10:37:44 +0200

    Log root URL in UriFactory

--- core/api/src/main/java/cloud/codestore/core/api/UriFactory.java
+++ core/api/src/main/java/cloud/codestore/core/api/UriFactory.java
@@ -1,5 +1,7 @@
 package cloud.codestore.core.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -11,12 +13,14 @@ import javax.annotation.Nonnull;
  */
 @Component
 public class UriFactory implements ApplicationListener<ServletWebServerInitializedEvent> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UriFactory.class);
     private static String ROOT_URI;
 
     @Override
     public void onApplicationEvent(ServletWebServerInitializedEvent event) {
         int port = event.getWebServer().getPort();
         ROOT_URI = "http://localhost:" + port;
+        LOGGER.info("Listening on " + ROOT_URI);
     }
 
     /**
