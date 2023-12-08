bca108d4ab9c6562ae18a06ac402e5a509f37a01
Franz Deschler <webmaster@codestore.cloud>
2023-12-08 16:13:24 +0100

    Dynamically change height of description text-area

--- client/client-ui/src/main/java/cloud/codestore/client/ui/snippet/description/SnippetDescription.java
+++ client/client-ui/src/main/java/cloud/codestore/client/ui/snippet/description/SnippetDescription.java
@@ -1,8 +1,10 @@
 package cloud.codestore.client.ui.snippet.description;
 
 import cloud.codestore.client.ui.FxController;
+import com.sun.javafx.scene.control.skin.Utils;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.text.TextBoundsType;
 
 @FxController
 public class SnippetDescription {
@@ -20,5 +22,17 @@ public class SnippetDescription {
 
     public void setText(String description) {
         snippetDescription.setText(description);
+        updateHeight();
+    }
+
+    private void updateHeight() {
+        double height = Utils.computeTextHeight(
+                snippetDescription.getFont(),
+                snippetDescription.getText(),
+                snippetDescription.getWidth(),
+                TextBoundsType.VISUAL
+        );
+
+        snippetDescription.setPrefHeight(height + 10);
     }
 }
