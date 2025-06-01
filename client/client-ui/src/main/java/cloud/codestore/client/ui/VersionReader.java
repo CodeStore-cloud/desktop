88285d55703f0ab6235e1955bc1d5f7d0c928fe8
Franz Deschler <webmaster@codestore.cloud>
2025-06-01 16:46:53 +0200

    Read version from properties file instead of manifest

--- client/client-ui/src/main/java/cloud/codestore/client/ui/VersionReader.java
+++ client/client-ui/src/main/java/cloud/codestore/client/ui/VersionReader.java
@@ -4,37 +4,25 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nonnull;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.jar.Attributes;
-import java.util.jar.JarInputStream;
-import java.util.jar.Manifest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
- * Reads the version of the application from the manifest file.
+ * Reads the version of the application from the corresponding properties file.
  */
 class VersionReader {
     private static final Logger LOGGER = LogManager.getLogger(VersionReader.class);
-    private static final String IMPLEMENTATION_VERSION = "Implementation-Version";
 
     @Nonnull
     String readVersion() {
-        String version = "";
-
-        try {
-            Path jarPath = Path.of(getClass().getProtectionDomain().getCodeSource().getLocation().toURI());
-            try (JarInputStream jar = new JarInputStream(Files.newInputStream(jarPath))) {
-                Manifest manifest = jar.getManifest();
-                Attributes attributes = manifest.getMainAttributes();
-
-                if (attributes != null && attributes.containsKey(new Attributes.Name(IMPLEMENTATION_VERSION))) {
-                    version = attributes.getValue(IMPLEMENTATION_VERSION);
-                }
-            }
-        } catch (Exception exception) {
-            LOGGER.warn("Unable to read version from manifest file.", exception);
+        Properties properties = new Properties();
+        try(InputStream in = getClass().getResourceAsStream("/version.properties")) {
+            properties.load(in);
+            return properties.getProperty("version", "");
+        } catch (IOException exception) {
+            LOGGER.warn("Unable to read version from properties file.", exception);
+            return "";
         }
-
-        return version;
     }
 }
