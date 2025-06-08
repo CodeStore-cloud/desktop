6f89bda16fc5467147d6d03770b36d216f3c708c
Franz Deschler <webmaster@codestore.cloud>
2025-06-08 11:04:06 +0200

    Show update notification in system tray

--- core/application/src/main/java/cloud/codestore/core/application/CodeStoreSystemTray.java
+++ core/application/src/main/java/cloud/codestore/core/application/CodeStoreSystemTray.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.Nullable;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ResourceBundle;
 
 /**
@@ -18,34 +19,61 @@ import java.util.ResourceBundle;
  * </ul>
  */
 @Component
-class CodeStoreSystemTray {
+public class CodeStoreSystemTray {
     private static final Logger LOGGER = LoggerFactory.getLogger(CodeStoreSystemTray.class);
     private static final ResourceBundle resourceBundle = ResourceBundle.getBundle("tray-messages");
 
-    private final SystemTray tray;
+    private final SystemTray systemTray;
+    private final TrayIcon trayIcon;
 
     @Autowired
     CodeStoreSystemTray(@Nullable SystemTray systemTray) {
-        tray = systemTray;
-        if (tray == null) {
+        this.systemTray = systemTray;
+        if (this.systemTray == null) {
             LOGGER.warn("System Tray is not supported");
+            trayIcon = null;
         } else {
             PopupMenu menu = createPopupMenu();
-            TrayIcon trayIcon = createTrayIcon(menu);
+            trayIcon = createTrayIcon(menu);
 
             try {
-                tray.add(trayIcon);
+                this.systemTray.add(trayIcon);
             } catch (AWTException exception) {
                 LOGGER.error("Failed to add tray icon", exception);
             }
         }
     }
 
+    public void showUpdateMessage() {
+        if (trayIcon != null) {
+            String title = resourceBundle.getString("tray.message.updateAvailable.title");
+            String message = resourceBundle.getString("tray.message.updateAvailable.message");
+            trayIcon.displayMessage(title, message, TrayIcon.MessageType.INFO);
+        }
+    }
+
+    public void setUpdateHandler(ActionListener listener) {
+        PopupMenu menu = trayIcon.getPopupMenu();
+        for (int i = 0; i < menu.getItemCount(); i++) {
+            MenuItem item = menu.getItem(i);
+            if ("update".equals(item.getName())) {
+                item.addActionListener(listener);
+                item.setEnabled(true);
+            }
+        }
+    }
+
     private PopupMenu createPopupMenu() {
         MenuItem exit = new MenuItem(resourceBundle.getString("tray.menu.exit"));
+        exit.setName("exit");
         exit.addActionListener(this::exit);
 
+        MenuItem update = new MenuItem(resourceBundle.getString("tray.menu.update"));
+        update.setName("update");
+        update.setEnabled(false);
+
         PopupMenu menu = new PopupMenu();
+        menu.add(update);
         menu.add(exit);
 
         return menu;
@@ -53,15 +81,15 @@ class CodeStoreSystemTray {
 
     private TrayIcon createTrayIcon(PopupMenu menu) {
         Image icon = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/trayicon.png"));
-        TrayIcon trayIcon = new TrayIcon(icon, "{CodeStore} Core", menu);
+        TrayIcon trayIcon = new TrayIcon(icon, "{CodeStore}", menu);
         trayIcon.setImageAutoSize(true);
         return trayIcon;
     }
 
     private void exit(ActionEvent e) {
-        TrayIcon[] trayIcons = tray.getTrayIcons();
+        TrayIcon[] trayIcons = systemTray.getTrayIcons();
         if (trayIcons.length > 0) {
-            tray.remove(trayIcons[0]);
+            systemTray.remove(trayIcons[0]);
         }
 
         System.exit(0);
--- core/application/src/test/java/cloud/codestore/core/application/CodeStoreSystemTrayTest.java
+++ core/application/src/test/java/cloud/codestore/core/application/CodeStoreSystemTrayTest.java
@@ -4,20 +4,25 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.util.ReflectionUtils;
 
 import java.awt.*;
+import java.lang.reflect.Field;
 import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("The system tray")
 class CodeStoreSystemTrayTest {
+    private static final int UPDATE_MENU_ITEM_INDEX = 0;
+    private static final int EXIT_MENU_ITEM_INDEX = 1;
+
     @Mock
     private SystemTray systemTray;
     private TrayIcon trayIcon;
+    private CodeStoreSystemTray codeStoreTray;
 
     @BeforeAll
     static void beforeAll() {
@@ -31,13 +36,41 @@ class CodeStoreSystemTrayTest {
             return null;
         }).when(systemTray).add(any(TrayIcon.class));
 
-        new CodeStoreSystemTray(systemTray);
+        codeStoreTray = new CodeStoreSystemTray(systemTray);
     }
 
     @Test
     @DisplayName("shows an icon")
     void showsIcon() {
         assertThat(trayIcon.getImage()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("enables the update menu item when setting the corresponding event listener")
+    void enableUpdateButton() {
+        MenuItem item = trayIcon.getPopupMenu().getItem(UPDATE_MENU_ITEM_INDEX);
+        assertThat(item.isEnabled()).isFalse();
+
+        codeStoreTray.setUpdateHandler(event -> {});
+
+        assertThat(item.isEnabled()).isTrue();
+    }
+
+    @Test
+    @DisplayName("displays update notification message")
+    void showsUpdateMessage() throws NoSuchFieldException {
+        TrayIcon spyTrayIcon = spy(trayIcon);
+        Field trayIconField = CodeStoreSystemTray.class.getDeclaredField("trayIcon");
+        ReflectionUtils.makeAccessible(trayIconField);
+        ReflectionUtils.setField(trayIconField, codeStoreTray, spyTrayIcon);
+
+        codeStoreTray.showUpdateMessage();
+
+        verify(spyTrayIcon).displayMessage(
+            "Update Available",
+            "There is an update available for {CodeStore}!",
+            TrayIcon.MessageType.INFO
+        );
     }
 
     @Nested
@@ -53,8 +86,19 @@ class CodeStoreSystemTrayTest {
         @Test
         @DisplayName("exit")
         void exitItem() {
-            MenuItem item = menu.getItem(0);
+            MenuItem item = menu.getItem(EXIT_MENU_ITEM_INDEX);
+            assertThat(item.getName()).isEqualTo("exit");
             assertThat(item.getLabel()).isEqualTo("Exit {CodeStore} Core");
+            assertThat(item.isEnabled()).isTrue();
+        }
+
+        @Test
+        @DisplayName("update")
+        void updateItem() {
+            MenuItem item = menu.getItem(UPDATE_MENU_ITEM_INDEX);
+            assertThat(item.getName()).isEqualTo("update");
+            assertThat(item.getLabel()).isEqualTo("Update");
+            assertThat(item.isEnabled()).isFalse();
         }
     }
 }
