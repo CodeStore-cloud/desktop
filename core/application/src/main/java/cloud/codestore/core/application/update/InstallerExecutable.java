4d67a976b8b4fa7c3b83da50862489f5631e58f0
Franz Deschler <webmaster@codestore.cloud>
2025-06-09 13:23:08 +0200

    Show progress for installer download

--- core/application/src/main/java/cloud/codestore/core/application/update/InstallerExecutable.java
+++ core/application/src/main/java/cloud/codestore/core/application/update/InstallerExecutable.java
@@ -4,24 +4,63 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 /**
  * Represents the Windows executable to install the application.
+ * This class handles downloading and saving the file.
  */
 class InstallerExecutable {
     private static final Logger LOGGER = LoggerFactory.getLogger(InstallerExecutable.class);
+    private static final int BUFFER_SIZE = 4096;
 
-    private final Path file;
+    private final long contentLength;
+    private final InputStream inputStream;
+    private Path file;
+    private ProgressListener progressListener = progress -> {};
+    private boolean canceled;
 
-    InstallerExecutable(@Nonnull byte[] fileContent) throws IOException {
-        Path tempDir = Files.createTempDirectory("codestore-update");
-        file = tempDir.resolve("CodeStore.exe");
-        Files.write(file, fileContent);
+    InstallerExecutable(long contentLength, @Nonnull InputStream inputStream) {
+        this.contentLength = contentLength;
+        this.inputStream = inputStream;
     }
 
+    /**
+     * Downloads the installer from the server.
+     * @throws IOException if the file could not be downloaded or saved on the local file system.
+     */
+    void download() throws IOException {
+        Path tempDir = Files.createTempDirectory("codestore-update");
+        file = tempDir.resolve("CodeStore.exe");
+
+        try (BufferedInputStream in = new BufferedInputStream(inputStream);
+             BufferedOutputStream fileStream = new BufferedOutputStream(new FileOutputStream(file.toFile()))
+        ) {
+            double totalBytesRead = 0;
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int bufferedBytes;
+
+            while(!canceled && (bufferedBytes = in.read(buffer)) != -1)
+            {
+                fileStream.write(buffer, 0, bufferedBytes);
+                totalBytesRead += bufferedBytes;
+                progressListener.onProgress(contentLength > 0 ? totalBytesRead / contentLength : 0);
+            }
+
+            fileStream.flush();
+        }
+
+        if(canceled) {
+            Files.deleteIfExists(file);
+        }
+    }
+
+    /**
+     * Executes the downloaded installer.
+     * @throws IOException if the file could not be executed.
+     */
     void execute() throws IOException {
         if (file != null && Files.exists(file)) {
             try {
@@ -33,5 +72,23 @@ class InstallerExecutable {
                 throw exception;
             }
         }
+    }
+
+    void setProgressListener(ProgressListener progressListener) {
+        this.progressListener = progressListener;
+    }
+
+    void cancelDownload() {
+        canceled = true;
+    }
+
+    /**
+     * A progress listener that is called as soon as a part of the file was downloaded.
+     */
+    interface ProgressListener {
+        /**
+         * @param progress the progress of the download between 0 and 1
+         */
+        void onProgress(double progress);
     }
 }
---
+++ core/application/src/test/java/cloud/codestore/core/application/update/InstallerExecutableTest.java
@@ -0,0 +1,98 @@
+package cloud.codestore.core.application.update;
+
+import org.junit.jupiter.api.*;
+import org.springframework.util.ReflectionUtils;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("The installer executable")
+class InstallerExecutableTest {
+
+    private static final byte[] TEST_CONTENT = randomContent();
+
+    private InstallerExecutable installer;
+
+    @BeforeEach
+    void setUp() {
+        InputStream inputStream = new ByteArrayInputStream(TEST_CONTENT);
+        installer = new InstallerExecutable(TEST_CONTENT.length, inputStream);
+    }
+
+    @Nested
+    @DisplayName("when downloading")
+    class DownloadingTest {
+        @AfterEach
+        void tearDown() throws NoSuchFieldException, IOException {
+            Files.deleteIfExists(getTempFile(installer));
+        }
+
+        @Test
+        @DisplayName("writes the content from the input stream to a file")
+        void writeInputStreamToFile() throws IOException, NoSuchFieldException {
+            installer.download();
+
+            Path tempFile = getTempFile(installer);
+            assertThat(tempFile).exists().hasBinaryContent(TEST_CONTENT);
+        }
+
+        @Test
+        @DisplayName("calls the progress listener while consuming the stream")
+        void callProgressListener() throws IOException {
+            AtomicInteger progressListenerCount = new AtomicInteger();
+            AtomicReference<Double> progress = new AtomicReference<>((double) 0);
+            installer.setProgressListener(p -> {
+                progressListenerCount.incrementAndGet();
+                progress.set(p);
+            });
+
+            installer.download();
+
+            assertThat(progressListenerCount.get()).isEqualTo(25);
+            assertThat(progress.get()).isEqualTo(1.0);
+        }
+
+        @Test
+        @DisplayName("cancels the download when the cancel flag was set")
+        void cancel() throws IOException, NoSuchFieldException {
+            AtomicBoolean cancelFlag = new AtomicBoolean(false);
+            installer.setProgressListener(progress -> {
+                assertThat(cancelFlag.get()).withFailMessage("Already canceled").isFalse();
+                if (progress > 0.5) {
+                    cancelFlag.set(true);
+                    installer.cancelDownload();
+                }
+            });
+
+            installer.download();
+
+            Path tempFile = getTempFile(installer);
+            assertThat(tempFile).doesNotExist();
+        }
+
+        private Path getTempFile(InstallerExecutable installer) throws NoSuchFieldException {
+            Field field = InstallerExecutable.class.getDeclaredField("file");
+            ReflectionUtils.makeAccessible(field);
+            return (Path) ReflectionUtils.getField(field, installer);
+        }
+    }
+
+    private static byte[] randomContent() {
+        int onHundredKilobytes = 100 * 1024;
+        byte[] data = new byte[onHundredKilobytes];
+
+        Random random = new Random();
+        random.nextBytes(data);
+        return data;
+    }
+}
