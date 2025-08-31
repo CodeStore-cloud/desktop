f662cedfdeb5ed9cf9dd5911bacbc18109f19070
Franz Deschler <webmaster@codestore.cloud>
2025-08-31 12:45:44 +0200

    Reformat UiMessages

--- client/client-ui/src/main/java/cloud/codestore/client/ui/UiMessages.java
+++ client/client-ui/src/main/java/cloud/codestore/client/ui/UiMessages.java
@@ -5,15 +5,13 @@ import java.text.MessageFormat;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
-public class UiMessages
-{
+public class UiMessages {
     private static final ResourceBundle MESSAGES = ResourceBundle.getBundle("uiMessages");
-    
+
     /**
      * @return the underlying {@link ResourceBundle} which contains the UI texts.
      */
-    static ResourceBundle bundle()
-    {
+    static ResourceBundle bundle() {
         return MESSAGES;
     }
 
@@ -21,21 +19,16 @@ public class UiMessages
      * Returns the message for the given key.
      * If one or more arguments are given, the message is formatted as defined in {@link MessageFormat}.
      *
-     * @param key a message key. Must not be {@code null}.
+     * @param key       a message key. Must not be {@code null}.
      * @param arguments optional arguments to pass into the message.
-     *
      * @return the formatted message from the resource bundle or the given key if there is no corresponding message.
      */
-    public static String get(@Nonnull String key, Object... arguments)
-    {
-        try
-        {
+    public static String get(@Nonnull String key, Object... arguments) {
+        try {
             String message = MESSAGES.getString(key);
             return arguments.length > 0 ? new MessageFormat(message).format(arguments) : message;
 
-        }
-        catch(MissingResourceException e)
-        {
+        } catch (MissingResourceException e) {
             return key;
         }
     }
