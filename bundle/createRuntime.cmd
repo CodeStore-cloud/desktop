6e788a1a0775be9efd3d3596acf586613c888d69
Franz Deschler <webmaster@codestore.cloud>
2026-02-08 14:01:30 +0100

    Automate build process

--- bundle/createRuntime.cmd
+++ bundle/createRuntime.cmd
@@ -1,9 +1,48 @@
 @echo off
 
-SET JDK_PATH="%1"
-SET JAVAFX_PATH="%2"
+SET JDK_VERSION=21.0.2
+SET JDK_URL=https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_windows-x64_bin.zip
+SET JAVAFX_VERSION=21.0.10
+SET JAVAFX_URL=https://download2.gluonhq.com/openjfx/%JAVAFX_VERSION%/openjfx-%JAVAFX_VERSION%_windows-x64_bin-jmods.zip
+
+SET TEMP_DIR=%LOCALAPPDATA%\Temp
+SET JDK_PATH=%TEMP_DIR%\jdk-%JDK_VERSION%
+SET JDK_JMODS=%JDK_PATH%\jmods
+SET JAVAFX_JMODS=%TEMP_DIR%\javafx-jmods-%JAVAFX_VERSION%
+
+:: ------------------------------------------------------------
+:: Download OpenJDK (cached)
+:: ------------------------------------------------------------
+SET ZIP_FILE="%TEMP_DIR%\jdk-%JDK_VERSION%.zip"
+if exist %JDK_PATH% (
+    echo Using cached OpenJDK
+) else (
+    echo Downloading OpenJDK %JDK_VERSION% ...
+    curl --output %ZIP_FILE% %JDK_URL%
+    tar -xf %ZIP_FILE% --directory "%TEMP_DIR%"
+    del %ZIP_FILE%
+)
+
+:: ------------------------------------------------------------
+:: Download JavaFX (cached)
+:: ------------------------------------------------------------
+SET ZIP_FILE="%TEMP_DIR%\javafx-%JAVAFX_VERSION%.zip"
+if exist %JAVAFX_JMODS% (
+    echo Using cached JavaFX
+) else (
+    echo Downloading JavaFX %JAVAFX_VERSION% jmods ...
+    curl --output %ZIP_FILE% %JAVAFX_URL%
+    tar -xf %ZIP_FILE% --directory "%TEMP_DIR%"
+    del %ZIP_FILE%
+)
+
+:: ------------------------------------------------------------
+:: Build Runtime
+:: ------------------------------------------------------------
+echo Creating runtime ...
+
 SET OUTPUT_PATH="target\application\runtime"
-SET MODULE_PATH="%JDK_PATH%\jmods;%JAVAFX_PATH%"
+SET MODULE_PATH="%JDK_JMODS%;%JAVAFX_JMODS%"
 SET MODULES=jdk.localedata,^
 java.logging,^
 java.net.http,^
@@ -16,15 +55,9 @@ javafx.controls,^
 javafx.fxml,^
 javafx.web
 
-if exist "%JAVA_HOME%\bin\jlink.exe" (
-    SET JLINK_EXE="%JAVA_HOME%\bin\jlink.exe"
-) else (
-    SET JLINK_EXE="%JAVA_HOME%\bin\jlink"
-)
-
 if exist %OUTPUT_PATH% rmdir /s /q %OUTPUT_PATH%
 
-%JLINK_EXE% ^
+%JDK_PATH%\bin\jlink.exe ^
 --module-path %MODULE_PATH% ^
 --add-modules %MODULES% ^
 --include-locales en,de ^
--- bundle/createRuntime.sh
+++ bundle/createRuntime.sh
@@ -1,17 +1,49 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ $# -lt 2 ]; then
-  echo "Usage: $0 <jdk-path> <javafx-path>"
-  exit 1
+JDK_VERSION=21.0.2
+JDK_URL=https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_linux-x64_bin.tar.gz
+JAVAFX_VERSION=21.0.10
+JAVAFX_URL=https://download2.gluonhq.com/openjfx/21.0.10/openjfx-21.0.10_linux-x64_bin-jmods.zip
+
+CACHE_DIR=~/.cache
+JDK_PATH=$CACHE_DIR/jdk-$JDK_VERSION
+JDK_JMODS=$JDK_PATH/jmods
+JAVAFX_JMODS=$CACHE_DIR/javafx-jmods-$JAVAFX_VERSION
+
+if [ ! -d $CACHE_DIR ]; then
+  mkdir $CACHE_DIR
 fi
 
-JDK_PATH=$1
-JAVAFX_PATH=$2
+# ------------------------------------------------------------
+# Download OpenJDK (cached)
+# ------------------------------------------------------------
+if [ -d "$JDK_PATH" ]; then
+  echo Using cached OpenJDK
+else
+  echo Downloading OpenJDK $JDK_VERSION ...
+  TAR_FILE=$CACHE_DIR/jdk-$JDK_VERSION.tar.gz
+  curl -o $TAR_FILE $JDK_URL
+  tar -xf $TAR_FILE -C $CACHE_DIR
+  rm -f $TAR_FILE
+fi
+
+# ------------------------------------------------------------
+# Download JavaFX (cached)
+# ------------------------------------------------------------
+
+if [ -d $JAVAFX_JMODS ]; then
+  echo Using cached JavaFX
+else
+  echo Downloading JavaFX $JAVAFX_VERSION jmods ...
+  TAR_FILE=$CACHE_DIR/javafx-$JAVAFX_VERSION.tar.gz
+  curl -L -o $TAR_FILE $JAVAFX_URL
+  tar -xf $TAR_FILE -C $CACHE_DIR
+  rm -f $TAR_FILE
+fi
 
 OUTPUT_PATH=target/application/runtime
-MODULE_PATH=$JDK_PATH/jmods:$JAVAFX_PATH
-
+MODULE_PATH=$JDK_JMODS:$JAVAFX_JMODS
 MODULES=\
 jdk.localedata,\
 java.logging,\
@@ -25,18 +57,11 @@ javafx.controls,\
 javafx.fxml,\
 javafx.web
 
-if [ -x "$JAVA_HOME/bin/jlink" ]; then
-  JLINK=$JAVA_HOME/bin/jlink
-else
-  echo "jlink not found in JAVA_HOME"
-  exit 1
+if [ -d $OUTPUT_PATH ]; then
+  rm -rf $OUTPUT_PATH
 fi
 
-if [ -d "$OUTPUT_PATH" ]; then
-  rm -rf "$OUTPUT_PATH"
-fi
-
-"$JLINK" \
+$JDK_PATH/bin/jlink \
   --module-path "$MODULE_PATH" \
   --add-modules "$MODULES" \
   --include-locales en,de \
